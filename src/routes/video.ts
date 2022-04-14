c4ef72c68a16beae60f4ee5262265367070f31e1
Sergey Yashanovsky <yashanovsky.sergey@ya.ru>
2022-04-14 22:57:31 +0500

    add video files router

--- src/routes/video.ts
+++ src/routes/video.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 import fs from 'fs';
 import { HydratedDocument } from 'mongoose';
+import path from 'path';
 import auth from '../middleware/auth';
 import { UserType } from '../models/User';
 
@@ -16,7 +17,7 @@ router.post('/video', auth, upload.single('file'), async (req, res) => {
     const user = res.locals.user as HydratedDocument<UserType>;
 
     // prevent attack
-    if (!file || !/^[a-f0-9-]+$/.test(file.originalname)) {
+    if (!file || !/^[a-zA-Zа-яА-Яё0-9-]+$/.test(file.originalname)) {
       res.status(400).send();
       return;
     }
@@ -34,6 +35,49 @@ router.post('/video', auth, upload.single('file'), async (req, res) => {
         res.status(err ? 500 : 201).send();
       },
     );
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.get('/video', auth, async (req, res) => {
+  try {
+    const user = res.locals.user as HydratedDocument<UserType>;
+    const dir = `${__dirname}/../db/video/${user.login}`;
+
+    fs.readdir(dir, (err, files) => {
+      if (err) {
+        res.status(500).send();
+        return;
+      }
+
+      const result = files.reduce((acc, file) => {
+        const stats = fs.statSync(`${dir}/${file}`);
+        acc.push({
+          name: path.parse(file).name,
+          size: stats.size,
+        });
+        return acc;
+      }, [] as { name: string; size: number; }[]);
+
+      res.json(result);
+    });
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.get('/video/:name', auth, async (req, res) => {
+  try {
+    const user = res.locals.user as HydratedDocument<UserType>;
+    const dir = `${__dirname}/../db/video/${user.login}`;
+    const { name } = req.params;
+
+    if (!name || !/^[a-zA-Zа-яА-Яё0-9-]+$/.test(name)) {
+      res.status(400).send();
+      return;
+    }
+    res.download(`${dir}/${name}.webm`);
   } catch (error) {
     res.status(500).send();
   }
